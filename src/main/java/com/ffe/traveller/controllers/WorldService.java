82de259cd2aee54a9aa7be1c520de5120dac692c
Sean Chitwood <darkmane@gmail.com>
2015-01-14 21:45:28 -0800

    Finished basic webservice setup.

--- src/main/java/com/ffe/traveller/controllers/WorldService.java
+++ src/main/java/com/ffe/traveller/controllers/WorldService.java
@@ -4,7 +4,9 @@ package com.ffe.traveller.controllers;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
+import javax.validation.constraints.*;
 /**
  * Created by darkmane on 1/13/15.
  */
@@ -14,8 +16,12 @@ public class WorldService {
 
 
     @GET
-    @Path("/search")
-    public Response searchAllWorlds(@PathParam("ruleSet") String rules) {
+    @Path("/")
+    public Response searchAllWorlds(@PathParam("ruleSet") String rules,
+                                    @QueryParam("sector") String sector,
+                                    @QueryParam("subsector") String subsector,
+                                    @QueryParam("hex") @Pattern("\d\d\d\d") String hex,
+                                    @QueryParam("upp") @Pattern("\w\w\w\w\w\w") String UPPs) {
         String output = "Jersey say : " + rules;
 
         return Response.status(200).entity(output).build();
